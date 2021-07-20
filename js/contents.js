6daa115a215b36f84c028f884c084bea65ecf182
Pyeong <tlvud12@gmail.com>
2021-07-21 00:48:52 +0900

    업데이트   - favicon 추가   - works페이지 변경   - contents페이지 템플릿 제작중

---
+++ js/contents.js
@@ -0,0 +1,30 @@
+
+function init() {
+  // When the user scrolls down 50px from the top of the document, resize the header's font size
+  window.onscroll = function () {
+    scrollFunction()
+  };
+
+  setTitleNavigation("Works");
+}
+
+function scrollFunction() {
+  var headerContainer = document.querySelector("#header");
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    headerContainer.style.fontSize = "1.7em";
+    headerContainer.style.padding = "1.5vh 1vh";
+  } else {
+    headerContainer.style.fontSize = "2em";
+    headerContainer.style.padding = "4vh 1vh";
+  }
+}
+
+function setTitleNavigation(currentPositionStr) {
+  const PAGE_NAME = "Pyeong"
+  var headerTitle = document.querySelector(".py");
+  headerTitle.innerHTML = `${PAGE_NAME} > ${currentPositionStr}`;
+}
+
+
+// INIT FUNCTION
+init();
